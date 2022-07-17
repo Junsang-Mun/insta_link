382efd28a9c91a6baf51bdda4052aa295b13c2d0
Junsang-mun <mun.js@aol.com>
2022-07-17 13:36:50 +0900

    그동안 firebase 및 .github 폴더를 ignore하고 있었다

---
+++ public/index.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+    let gh = document.getElementById("link_gh");
+    let blog = document.getElementById("link_blog");
+    gh.onclick = openGh;
+    blog.onclick = openBlog;
+};
+
+function openGh() {
+    window.open("https://github.com/junsang-mun", "_blank").focus();
+}
+
+function openBlog() {
+    window.open("https://junsang-mun.github.io", "_blank").focus();
+}
